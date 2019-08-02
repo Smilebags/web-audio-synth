2fdc098cc302ec780958db7062ab51cda239b857
Chris Cook <christopher.cook@outlook.com.au>
2019-08-03 00:19:27 +1000

    Add variable frequency to oscillator

--- src/modules/OscillatorModule.ts
+++ src/modules/OscillatorModule.ts
@@ -1,5 +1,7 @@
 import Plug from "../Plug.js";
 import AbstractRackModule from "./AbstractRackModule.js";
+import { Vec2 } from "../types.js";
+import { subtract } from "../util.js";
 
 export default class OscillatorModule extends AbstractRackModule {
   width!: number;
@@ -8,6 +10,11 @@ export default class OscillatorModule extends AbstractRackModule {
   name: string = 'Osc';
   private osc: OscillatorNode;
   private vo: AudioWorkletNode;
+
+  private initialFreq: number | null = null;
+  private mousedownPos: Vec2 | null = null;
+  private mousemovePos: Vec2 | null = null;
+
   constructor(context: AudioContext, type: OscillatorType = 'sine', startingFrequency: number = 440) {
     super();
 
@@ -21,5 +28,27 @@ export default class OscillatorModule extends AbstractRackModule {
 
     this.addPlug(this.vo, 'V/O In', 'in', 0);
     this.addPlug(this.osc, 'Out', 'out', 2);
+
+    this.addEventListener('mousedown', (e: Vec2) => {this.handleMousedown(e)});
+    this.addEventListener('mousemove', (e: Vec2) => {this.handleMousemove(e)});
+  }
+
+  handleMousedown(mousedownEvent: Vec2): void {
+    if (!this.isInFreqBox(mousedownEvent)) {
+      return;
+    }
+    this.mousedownPos = mousedownEvent;
+    this.initialFreq = this.osc.frequency.value;
+  }
+  handleMousemove(mousemoveEvent: Vec2): void {
+    this.mousemovePos = mousemoveEvent;
+    if (!this.mousedownPos || !this.initialFreq) {
+      return;
+    }
+    const relativeYPos = subtract(this.mousedownPos, this.mousemovePos).y;
+    this.osc.frequency.value = this.initialFreq + (relativeYPos * 2 );
+  }
+  isInFreqBox(pos: Vec2): boolean {
+    return pos.y >= 200;
   }
 }
