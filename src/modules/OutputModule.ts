c6cca417dd5f818dd49a7e4583b1187b3f9a8238
Chris Cook <christopher.cook@outlook.com.au>
2019-09-13 21:14:42 +1000

    Upgrade output module

--- src/modules/OutputModule.ts
+++ src/modules/OutputModule.ts
@@ -5,10 +5,16 @@ export default class OutputModule extends AbstractRackModule {
   width!: number;
   context: AudioContext;
   plugs!: Plug[];
-  type: string = 'System Out';
+  type: string = 'Output';
   constructor(context: AudioContext) {
     super();
     this.context = context;
     this.addPlug(this.context.destination, '', 'in');
+  }
+
+  toParams(): any {
+    return {
+      type: this.type,
+    };
   }
 }
