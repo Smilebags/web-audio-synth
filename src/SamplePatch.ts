bae8a55b0a707a2364cc188f4bd6dda68b909ba1
Chris Cook <christopher.cook@outlook.com.au>
2019-09-13 22:00:37 +1000

    Move sample patch to new file

---
+++ src/SamplePatch.ts
@@ -0,0 +1,90 @@
+export default `{
+  "moduleSlots": [
+    {
+      "module": {
+        "type": "Output"
+      },
+      "position": {
+        "x": 0,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Oscillator",
+        "oscType": "sine",
+        "voltageOffset": 6.78135971352466
+      },
+      "position": {
+        "x": 100,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Oscillator",
+        "oscType": "sine",
+        "voltageOffset": 8.78135971352466
+      },
+      "position": {
+        "x": 200,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Delay",
+        "startingDelay": 0.20000000298023224
+      },
+      "position": {
+        "x": 300,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Envelope",
+        "a": 0.009999999776482582,
+        "d": 0.20000000298023224,
+        "s": 1,
+        "r": 0.20000000298023224
+      },
+      "position": {
+        "x": 400,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Filter",
+        "voltageOffset": 8.78135971352466
+      },
+      "position": {
+        "x": 500,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "Gain",
+        "gain": 1
+      },
+      "position": {
+        "x": 600,
+        "y": 20
+      }
+    },
+    {
+      "module": {
+        "type": "KeyboardInput",
+        "isOn": true,
+        "octave": 0,
+        "gateHighVoltage": 1
+      },
+      "position": {
+        "x": 700,
+        "y": 20
+      }
+    }
+  ]
+}`;
