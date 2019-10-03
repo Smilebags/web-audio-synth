5928f8ba3ae05f061c053f74a528c678f7cb5698
Chris Cook <christopher.cook@outlook.com.au>
2019-10-03 23:06:06 +1000

    Update sample patch

--- src/SamplePatch.ts
+++ src/SamplePatch.ts
@@ -1,107 +1,43 @@
-export default `{
-  "moduleSlots": [
+export default JSON.stringify({
+  moduleSlots: [
     {
-      "module": {
-        "type": "Output"
+      module: {
+        type: "Output"
       },
-      "position": {
-        "x": 0,
-        "y": 0
+      position: {
+        x: 0,
+        y: 0
       }
     },
     {
-      "module": {
-        "type": "KeyboardInput",
-        "isOn": true,
-        "octave": 0,
-        "gateHighVoltage": 1
+      module: {
+        type: "KeyboardInput",
+        isOn: true,
+        octave: 0,
+        gateHighVoltage: 1
       },
-      "position": {
-        "x": 100,
-        "y": 0
+      position: {
+        x: 100,
+        y: 0
       }
     },
     {
-      "module": {
-        "type": "VoltageSequencer",
-        "stepCount": 64
+      module: {
+        type: "MidiInput",
       },
-      "position": {
-        "x": 200,
-        "y": 0
+      position: {
+        x: 200,
+        y: 0
       }
     },
     {
-      "module": {
-        "type": "Reverb"
+      module: {
+        type: "MidiCCInput"
       },
-      "position": {
-        "x": 300,
-        "y": 0
+      position: {
+        x: 300,
+        y: 0
       }
     },
-    {
-      "module": {
-        "type": "Filter",
-        "voltageOffset": 8.78135971352466
-      },
-      "position": {
-        "x": 400,
-        "y": 0
-      }
-    },
-    {
-      "module": {
-        "type": "Delay",
-        "startingDelay": 0.20000000298023224
-      },
-      "position": {
-        "x": 500,
-        "y": 0
-      }
-    },
-    {
-      "module": {
-        "type": "Envelope",
-        "a": 0.009999999776482582,
-        "d": 0.20000000298023224,
-        "s": 1,
-        "r": 0.20000000298023224
-      },
-      "position": {
-        "x": 600,
-        "y": 0
-      }
-    },
-    {
-      "module": {
-        "type": "Oscillator",
-        "oscType": "sawtooth",
-        "voltageOffset": 8.78135971352466
-      },
-      "position": {
-        "x": 700,
-        "y": 0
-      }
-    },
-    {
-      "module": {
-        "type": "Gain",
-        "gain": 0
-      },
-      "position": {
-        "x": 800,
-        "y": 0
-      }
-    },
-    {
-      "module": {
-        "type": "VoltageQuantizer"
-      },
-      "position": {
-        "x": 900,
-        "y": 0
-      }
-    }
   ]
-}`;
+});
