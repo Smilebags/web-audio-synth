c9a3b8a715b4c4e818d412eb802f58350069bc8d
Chris Cook <christopher.cook@outlook.com.au>
2019-10-05 01:01:13 +1000

    Add extra input params

--- src/modules/MidiInputModule.ts
+++ src/modules/MidiInputModule.ts
@@ -10,6 +10,10 @@ export default class MidiInputModule extends AbstractRackModule {
   name = "Midi In";
 
   private gate: ConstantSourceNode;
+  private velocity: ConstantSourceNode;
+  private trigger: ConstantSourceNode;
+  private pitchBend: ConstantSourceNode;
+  private modWheel: ConstantSourceNode;
   private vo: ConstantSourceNode;
   private gateHighVoltage: number;
   private currentNotes: Set<number>;
@@ -32,9 +36,25 @@ export default class MidiInputModule extends AbstractRackModule {
     this.gateHighVoltage = gateHighVoltage;
     this.currentNotes = new Set();
 
+    this.velocity = this.context.createConstantSource();
+    this.velocity.offset.value = 0;
+    this.velocity.start();
+
     this.gate = this.context.createConstantSource();
     this.gate.offset.value = 0;
     this.gate.start();
+
+    this.trigger = this.context.createConstantSource();
+    this.trigger.offset.value = 0;
+    this.trigger.start();
+
+    this.pitchBend = this.context.createConstantSource();
+    this.pitchBend.offset.value = 0;
+    this.pitchBend.start();
+
+    this.modWheel = this.context.createConstantSource();
+    this.modWheel.offset.value = 0;
+    this.modWheel.start();
 
     this.vo = this.context.createConstantSource();
     this.vo.offset.value = 0;
@@ -42,8 +62,12 @@ export default class MidiInputModule extends AbstractRackModule {
 
     this.setupMidiAccess();
 
-    this.addPlug(this.gate, "Gate", "out");
     this.addPlug(this.vo, "V/O", "out");
+    this.addPlug(this.velocity, "Velocity", "out");
+    this.addPlug(this.gate, "Gate", "out");
+    this.addPlug(this.trigger, "Trigger", "out");
+    this.addPlug(this.pitchBend, "Pitchbend", "out");
+    this.addPlug(this.modWheel, "Modwheel", "out");
 }
 
   async setupMidiAccess() {
@@ -63,12 +87,19 @@ export default class MidiInputModule extends AbstractRackModule {
   }
 
   handleMidiMessage(e:any ): void {
+    console.log(e.data);
     switch (e.data[0]) {
       case 144:
         this.handleNoteOn(e.data);
         break;
       case 128:
         this.handleNoteOff(e.data);
+        break;
+      case 224:
+        this.handlePitchBend(e.data);
+        break;
+      case 176:
+        this.handleModWheel(e.data);
         break;
       default:
         break;
@@ -79,8 +110,12 @@ export default class MidiInputModule extends AbstractRackModule {
     this.currentNotes.add(data[1]);
     const keyVoltage = this.voltageFromKeyNumber(data[1]);
     this.setVOVoltage(keyVoltage);
+    const keyVelocity = data[2] / 127;
+    this.setVelocity(keyVelocity);
     this.setGate(true);
+    this.handleTrigger();
   }
+
   handleNoteOff(data: [128, number, number]) {
     if (!this.currentNotes.has(data[1])) {
       return;
@@ -95,12 +130,36 @@ export default class MidiInputModule extends AbstractRackModule {
     this.setVOVoltage(keyVoltage);
   }
 
+  handlePitchBend(data: [224, number, number]) {
+    const pitchbendValue = data[2] > 0
+      ? (data[2] - 64) / 63
+      : (data[2] - 64) / 64;
+    this.pitchBend.offset.value = pitchbendValue;
+  }
+  handleModWheel(data: [224, number, number]) {
+    if (data[1] !== 1) {
+      return;
+    }
+    this.modWheel.offset.value = data[2] / 127;
+  }
+
   voltageFromKeyNumber(keyNumber: number): number {
     return (keyNumber - 69) / 12;
   }
 
+  handleTrigger() {
+    this.trigger.offset.value = this.gateHighVoltage;
+    setTimeout(() => {
+      this.trigger.offset.value = 0;
+    }, 5);
+  }
+
   setVOVoltage(voltage: number) {
     this.vo.offset.value = voltage;
+  }
+
+  setVelocity(voltage: number) {
+    this.velocity.offset.value = voltage;
   }
 
   setGate(onOff: boolean): void {
