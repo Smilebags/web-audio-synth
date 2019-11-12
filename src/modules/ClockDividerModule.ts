2d0bf8c29709fef048d7b36b937f431b14c14678
Chris Cook <christopher.cook@outlook.com.au>
2019-11-13 00:49:21 +1100

    Add extra levels to clock divider

--- src/modules/ClockDividerModule.ts
+++ src/modules/ClockDividerModule.ts
@@ -11,31 +11,38 @@ export default class ClockDividerModule extends AbstractRackModule {
   private div4: GainNode;
   private div8: GainNode;
   private div16: GainNode;
+  private div32: GainNode;
+  private div64: GainNode;
 
   constructor(context: AudioContext) {
     super();
     this.context = context;
-    this.divider = new AudioWorkletNode(this.context, 'clock-divider-processor', {numberOfOutputs: 4});
+    this.divider = new AudioWorkletNode(this.context, 'clock-divider-processor', {numberOfOutputs: 6});
 
     this.div2 = this.context.createGain();
     this.div4 = this.context.createGain();
     this.div8 = this.context.createGain();
     this.div16 = this.context.createGain();
+    this.div32 = this.context.createGain();
+    this.div64 = this.context.createGain();
 
     this.divider.connect(this.div2, 0);
     this.divider.connect(this.div4, 1);
     this.divider.connect(this.div8, 2);
     this.divider.connect(this.div16, 3);
+    this.divider.connect(this.div32, 4);
+    this.divider.connect(this.div64, 5);
 
     this.addPlug(this.divider, 'In', 'in');
-    const resetTriggerParam = this.divider.parameters.get('resetTrigger');
-    if(resetTriggerParam) {
-      this.addPlug(resetTriggerParam, 'Reset', 'in');
-    }
+    const resetTriggerParam = this.divider.parameters.get('resetTrigger')!;
+    this.addPlug(resetTriggerParam, 'Reset', 'in');
+
     this.addPlug(this.div2, '/2', 'out');
     this.addPlug(this.div4, '/4', 'out');
     this.addPlug(this.div8, '/8', 'out');
     this.addPlug(this.div16, '/16', 'out');
+    this.addPlug(this.div32, '/32', 'out');
+    this.addPlug(this.div64, '/64', 'out');
   }
 
   toParams(): any {
--- src/processors/ClockDividerProcessor.js
+++ src/processors/ClockDividerProcessor.js
@@ -4,13 +4,13 @@ class ClockDividerProcessor extends AudioWorkletProcessor {
     this.isHigh = false;
     this.isResetHigh = false;
     this.threshold = 0.1;
-    this.currentStep = 15;
+    this.currentStep = 63;
     this.isPreStart = true;
     this.cutoffValue = 0.5;
   }
 
   tick() {
-    this.currentStep = (this.currentStep + 1) % 16;
+    this.currentStep = (this.currentStep + 1) % 64;
   }
 
   reset() {
@@ -23,6 +23,8 @@ class ClockDividerProcessor extends AudioWorkletProcessor {
     const outputChannel2 = outputs[1][0];
     const outputChannel3 = outputs[2][0];
     const outputChannel4 = outputs[3][0];
+    const outputChannel5 = outputs[4][0];
+    const outputChannel6 = outputs[5][0];
     const input = inputs[0];
     const inputChannel = input[0];
     
@@ -52,10 +54,12 @@ class ClockDividerProcessor extends AudioWorkletProcessor {
       }
       
       // set output
-      outputChannel1[i] = this.currentStep % 2 === 0;
-      outputChannel2[i] = this.currentStep % 4 <= 1;
-      outputChannel3[i] = this.currentStep % 8 <= 3;
-      outputChannel4[i] = this.currentStep % 16 <= 7;
+      outputChannel1[i] = this.currentStep % (2 ** 1) === 0;
+      outputChannel2[i] = this.currentStep % (2 ** 2) < (2 ** 1);
+      outputChannel3[i] = this.currentStep % (2 ** 3) < (2 ** 2);
+      outputChannel4[i] = this.currentStep % (2 ** 4) < (2 ** 3);
+      outputChannel5[i] = this.currentStep % (2 ** 5) < (2 ** 4);
+      outputChannel6[i] = this.currentStep % (2 ** 6) < (2 ** 5);
     }
 
     return true;
